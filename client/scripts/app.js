0077517f742f7f4c06aae7bd1ab460eeeaa8abdb
yesplease <bradley.heinz@gmail.com>
2015-02-17 15:00:53 -0800

    All requirements pass, though specRunner tests not all passing. Moving on to refactor into Backbone instead.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,35 +1,60 @@
 var app = {
   username: window.location.search.substr(10),
   text: "Beware.",
-  roomname: "4chan"
+  roomname: window.prompt("Which room do you want to join? Lobby or Everywhere?"),
+  friends: [],
+  uniq: [],
+  // server: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+  server: 'https://api.parse.com/1/classes/chatterbox'
 }
 
+var message = {
+  username: app.username,
+  text: "initial",
+  roomname: app.roomname
+};
 
-var getChats = function(){
+app.clearMessages = function(){
+  $('#chats').empty();
+};
+
+app.addMessage = function(msg){
+  app.send(msg);
+};
+
+
+
+app.fetch = function(room){
  $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+    url: app.server,
     type: 'GET',
     contentType: 'application/json',
+
+    data: {
+      where:{"roomname": room},
+      order:"-createdAt"
+      },
     success: function (data) {
-      console.log(data);
+      console.dir(data.results);
       displayChats(data.results);
     } ,
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
+      console.error('chatterbox: Failed to get messages');
     }
   });
-};
+}
 
-var submitChat = function(chat){
+
+app.send = function(chat){
   $.ajax({
     // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
+    url: app.server,
     type: 'POST',
     data: JSON.stringify(chat),
     contentType: 'application/json',
     success: function (data) {
-      getChats();
+      app.fetch(app.roomname);
     } ,
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -38,29 +63,58 @@ var submitChat = function(chat){
   });
 };
 
-
-window.uniq = [];
+app.init = function(){
+  app.fetch(this.roomname);
+};
 
 var displayChats = function(chatsArray){
   for (var i = chatsArray.length - 1; i >= 0; i--){
-    if(uniq.indexOf(chatsArray[i].objectId) === -1){
-      var chat = _.escape(chatsArray[i].username + ': ' + chatsArray[i].text);
-      $('#chats').prepend("<li>" + chat + "</li>");
-      //console.log(chatsArray[i].objectId);
-      uniq.push(chatsArray[i].objectId);
+    if(app.uniq.indexOf(chatsArray[i].objectId) === -1){
+      var escName = _.escape(chatsArray[i].username);
+      var escText = _.escape(chatsArray[i].text);
+      var index = app.friends.indexOf(escName);
+      if (index === -1){
+        $('#chats').prepend("<li><a class ='stranger'>" + escName + "</a>: " + escText + "</li>");
+
+
+      } else {
+        $('#chats').prepend("<li><a class ='friend'>" + escName + "</a>: " + escText + "</li>");
+      }
+      app.uniq.push(chatsArray[i].objectId);
     }
   }
+  $("a").on("click", function(){
+    var clickedName = $(this).text();
+    var index = app.friends.indexOf(clickedName);
+    if (index === -1){
+      app.friends.push(clickedName);
+    } else {
+      app.friends.splice(index, 1);
+    }
+    $("a:contains('" + clickedName + "')" ).toggleClass("friend");
+    console.log(app.friends)
+  });
+
 };
 
-//form submit
-$("#refresher").click(getChats);
 
-$('#send').click(function() {
-  var input = $('#message').val();
-  app.text = input;
-  submitChat(app);
-  $('#message').val("");
+//form submit
+$("#refresher").click(function(){
+  app.fetch(app.roomname);
 });
 
 
+$('#send').click(function() {
+  var input = $('#message').val();
+  message.text = input;
+  app.send(message);
+  $('#message').val("");
+});
 
+$('#changeRoom').click(function() {
+  app.roomname = window.prompt("Which room do you want to join? Lobby or Everywhere?");
+  app.fetch(app.roomname);
+});
+
+
+$(document).ready(app.init());
